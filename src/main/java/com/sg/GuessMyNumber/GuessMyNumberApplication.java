1cc6e8018e68b258b86cf97d1ac4f46d1ad3e939
maisow <msow@uwaterloo.ca>
2023-07-10 14:54:50 -0400

    Spring Initializer

---
+++ src/main/java/com/sg/GuessMyNumber/GuessMyNumberApplication.java
@@ -0,0 +1,13 @@
+package com.sg.GuessMyNumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessMyNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessMyNumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sg/GuessMyNumber/GuessMyNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.GuessMyNumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessMyNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
