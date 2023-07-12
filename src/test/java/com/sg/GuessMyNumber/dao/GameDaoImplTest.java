666515fa3baefb97748461388abb5199c181783c
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2023-07-12 14:55:52 -0400

    test passes for addAndGetAllGames

--- src/test/java/com/sg/GuessMyNumber/dao/GameDaoImplTest.java
+++ src/test/java/com/sg/GuessMyNumber/dao/GameDaoImplTest.java
@@ -1,33 +1,80 @@
 package com.sg.GuessMyNumber.dao;
 
+import com.sg.GuessMyNumber.GuessMyNumberApplication;
+import com.sg.GuessMyNumber.dto.Game;
+import com.sg.GuessMyNumber.dto.Round;
+import com.sg.GuessMyNumber.service.GameService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GuessMyNumberApplication.class)
 class GameDaoImplTest {
+    private JdbcTemplate jdbcTemplate;
+    private GameDao gameDao;
+    private RoundDao roundDao;
+    @Autowired
+    public void GameDaoImplTest(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        gameDao = new GameDaoImpl(jdbcTemplate);
+    }
+    public GameDaoImplTest() {
+    }
 
     @BeforeEach
     void setUp() {
+        List<Game> games = gameDao.getAllGames();
+        for (Game game : games) {
+            gameDao.deleteGameById(game.getGameId());
+        }
+
+//        List<Round> rounds = roundDao.getAllRounds();
+//        for (Round round : rounds) {
+//            gameDao.deleteGameById(round.getRoundId());
+//        }
+    }
+
+//    @Test
+//    void addGame () {
+//
+//    }
+
+    @Test
+    void addAndGetAllGames () {
+        //implemented
+        GameService gameService = new GameService(gameDao, roundDao);
+        Game game = gameService.newGame();
+        Game game2 = gameService.newGame();
+        gameDao.addGame(game);
+        gameDao.addGame(game2);
+        List<Game> games = gameDao.getAllGames();
+
+        assertEquals(2, games.size());
+        assertTrue(games.contains(game));
+        assertTrue(games.contains(game2));
+
+
     }
 
     @Test
-    void getAllGames() {
+    void getGameById () {
+    }
+
+
+    @Test
+    void updateGame () {
     }
 
     @Test
-    void getGameById() {
-    }
-
-    @Test
-    void addGame() {
-    }
-
-    @Test
-    void updateGame() {
-    }
-
-    @Test
-    void deleteGameById() {
+    void deleteGameById () {
     }
 }
